eb1199d6b4ec76fc7fa3ee252b258bafeaaa0327
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-12-18 18:22:57 +0100

    init

---
+++ src/test/kotlin/com/example/SpringSimpleTest/SpringSimpleTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SpringSimpleTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSimpleTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
